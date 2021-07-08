0ec061556f41e591daaea2e8be7682d585e33933
John Brilhart <jbrilhar@gmail.com>
2021-07-08 18:51:25 -0400

    Added base project with nav bar

---
+++ HealthyShopper/app/src/main/java/pace/cs639/healthyshopper/MainActivity.java
@@ -0,0 +1,31 @@
+package pace.cs639.healthyshopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);    //Initialize Bottom Navigation View.
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        //Pass the ID's of Different destinations
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_pantry, R.id.navigation_shopping_list, R.id.navigation_settings)
+                .build();
+
+        //Initialize NavController.
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+}
